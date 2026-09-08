4b28f6dad14ed99187696f066282a325f97dfebd
ammarsarhan <ammarsarhan06@gmail.com>
2026-09-08 21:47:29 +0300

    Modified the Navbar component to be fully responsive.

--- ui/components/Navbar.tsx
+++ ui/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import Button from "@/components/Button";
-import { IconChevronDown, IconWorld } from "@tabler/icons-react";
+import { IconChevronDown, IconMenu, IconWorld } from "@tabler/icons-react";
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between py-4 px-20 bg-black">
+        <nav className="flex items-center justify-between py-4 px-8 lg:px-20 bg-black">
             <div className="flex items-center gap-x-16">
                 <Link href="/">
                     <div className="size-6 bg-white"></div>
                 </Link>
-                <div className="flex items-center gap-x-3">
+                <div className="hidden lg:flex items-center gap-x-3">
                     <Link href="/">
                         <Button variant="primary">
                             <span>Contact</span>
@@ -31,7 +31,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div className="flex items-center gap-x-3">
-                <div className="flex items-center gap-x-2">
+                <div className="hidden lg:flex items-center gap-x-2">
                     <Link href={"/"}>
                         <Button variant="primary">
                             <div className="flex items-center gap-x-1.5">
@@ -52,6 +52,9 @@ export default function Navbar() {
                         <Button variant="secondary">Sign Up</Button>
                     </Link>
                 </div>
+                <button className="flex lg:hidden items-center justify-center size-10 cursor-pointer rounded-full text-white bg-black hover:bg-gray-900 transition-all">    
+                    <IconMenu size={20} strokeWidth={2.5}/>
+                </button>   
             </div>
         </nav>
     )
